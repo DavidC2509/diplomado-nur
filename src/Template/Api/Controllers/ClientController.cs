1576f4ee666b448696de73c475015c21bd5e1d56
DavidC2509 <davidfernando.chavez777@gmail.com>
2025-05-11 17:56:35 -0400

    Cambio url

--- src/Template/Api/Controllers/ClientController.cs
+++ src/Template/Api/Controllers/ClientController.cs
@@ -16,7 +16,7 @@ namespace Template.Api.Controllers
         ///<summary>
         ///Listado Clientes
         ///</summary>
-        [HttpGet("list")]
+        [HttpGet("list/test/deploy")]
         public Task<ActionResult<IEnumerable<ClientModel>>> ListClient() => SendRequest(new ListClientQuery());
 
         ///<summary>
