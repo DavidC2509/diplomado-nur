6d1c5e09ba699caf728e6eb0e1943471020cfe2a
DavidC2509 <davidfernando.chavez777@gmail.com>
2025-06-06 18:02:59 -0400

    cambios azure

--- src/Template/Migration/Program.cs
+++ src/Template/Migration/Program.cs
@@ -18,7 +18,7 @@ builder.ConfigureContainer(new AutofacServiceProviderFactory(), containerBuilder
 {
     containerBuilder.RegisterModule(new DefaultInfrastructureModule(builder.Environment.EnvironmentName == "Development"));
 });
-builder.Services.AddDbContext("Host=database-postgres.postgres.database.azure.com;Port=5432;Database=david2;Username=nutripostgres;Password=LinkinPark#2025;Ssl Mode=Require;Trust Server Certificate=true");
+builder.Services.AddDbContext("UseEnvironmentVariable");
 
 builder.EnrichNpgsqlDbContext<DataBaseContext>(settings =>
 // Disable Aspire default retries as we're using a custom execution strategy
