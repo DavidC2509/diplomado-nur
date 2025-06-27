e581d0c355265cb13e1928ab6223457f2ad86da6
DavidC2509 <davidfernando.chavez777@gmail.com>
2025-06-27 00:32:54 -0400

    Modificaciones

---
+++ src/Template/Api/Extensions/RoleNameTelemetryInitializer.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace Template.Api.Extensions
+{
+    public class RoleNameTelemetryInitializer : ITelemetryInitializer
+    {
+        private readonly string _roleName;
+
+        public RoleNameTelemetryInitializer(string roleName)
+        {
+            _roleName = roleName;
+        }
+
+        public void Initialize(ITelemetry telemetry)
+        {
+            telemetry.Context.Cloud.RoleName = _roleName;
+        }
+    }
+}
+
